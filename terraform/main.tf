f74c27f29a182dc473bcc315afaa41443ae5ade6
VERDOM <maxence.verdom@cgi.com>
2026-01-29 15:36:45 +0100

    copie provider du bucket vers dossier terraform et pas à la racine du projet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0"
-    }
-  }
-}
-
 # resource "google_project_service" "services" {
 #   for_each = toset([
 #     "run.googleapis.com",
